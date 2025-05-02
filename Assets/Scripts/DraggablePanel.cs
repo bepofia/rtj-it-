475925df710008eba85bd410c905ba5c069bd140
bepofia <barkodstorm@gmail.com>
2025-05-03 00:28:27 +0300

    panel

--- Assets/Scripts/DraggablePanel.cs
+++ Assets/Scripts/DraggablePanel.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class DraggablePanel : MonoBehaviour
+public class DraggablePanel : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private Vector2 pointerOffset;
+
+    [Header("DOTween Ayarlarý")]
+    public float tweenSpeed = 0.1f;
+    public Ease tweenEase = Ease.OutQuad;
+
+    private void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        Vector2 localMousePos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out localMousePos
+        );
+
+        pointerOffset = rectTransform.anchoredPosition - localMousePos;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 localPointerPosition;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out localPointerPosition))
+        {
+            Vector2 targetPos = localPointerPosition + pointerOffset;
+            rectTransform.DOKill(); // önceki tween’i iptal et
+            rectTransform.DOAnchorPos(targetPos, tweenSpeed).SetEase(tweenEase);
+        }
     }
 }
